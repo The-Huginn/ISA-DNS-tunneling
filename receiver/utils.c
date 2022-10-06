34eb631f56c0720bc934e0b7641f2d85ed7b56d2
The-Huginn <rastislavbudinsky@gmail.com>
2022-10-06 19:54:44 +0200

    Fix some bugs with receiver

--- receiver/utils.c
+++ receiver/utils.c
@@ -8,25 +8,97 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-
-int openUDP(struct sockaddr* server) {
-    if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+int openUDP(struct sockaddr *server)
+{
+    if ((udp = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         fprintf(stderr, "socket() failed");
         return false;
     }
 
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1)
+    if (setsockopt(udp, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1)
     { // maybe SO_REUSEPORT needed, not working now
         fprintf(stderr, "Unable to access port\n");
         return false;
     }
 
-    if (bind(fd, server, sizeof(struct sockaddr_in)) == -1)
+    if (bind(udp, server, sizeof(struct sockaddr_in)) == -1)
     {
         fprintf(stderr, "bind() failed");
-        close(fd);
+        close(udp);
+        return false;
+    }
+
+    return true;
+}
+
+int openTCP(struct sockaddr *server)
+{
+    if ((tcp = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+    {
+        fprintf(stderr, "socket(): could not create the socket\n");
+        return false;
+    }
+
+    if (bind(tcp, (struct sockaddr *)server, sizeof(struct sockaddr_in)) < 0)
+    {
+        fprintf(stderr, "TCP binding failed'n");
+        return false;
+    }
+
+    if (setsockopt(tcp, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1)
+    {
+        fprintf(stderr, "Unable to access port\n");
+        return false;
+    }
+
+    if (listen(tcp, 1) == -1)
+    {
+        fprintf(stderr, "TCP listen failed\n");
+        return false;
+    }
+
+    return true;
+}
+
+unsigned char *readPayload(unsigned char *buffer, int *msg_size)
+{
+    unsigned char qname[STRING_SIZE + 1] = {'\0'}, file[STRING_SIZE + 1] = {'\0'};
+    // copy qname
+    strcpy(qname, &buffer[HEADER_SIZE]);
+
+    unsigned char *payload = &buffer[HEADER_SIZE + strlen(qname) + 1 + sizeof(question)];
+
+    payload = &payload[strlen(file) + 1];
+    msg_size -= (unsigned char *)buffer - (unsigned char *)payload;
+    // msg_size -= HEADER_SIZE + strlen(qname) + 1 + sizeof(question) + strlen(file) + 1;
+
+    return payload;
+}
+
+int openFile(unsigned char *path, unsigned char *buffer)
+{
+    unsigned char qname[STRING_SIZE + 1] = {'\0'}, file[STRING_SIZE + 1] = {'\0'};
+    // copy qname
+    strcpy(qname, &buffer[HEADER_SIZE]);
+
+    unsigned char *payload = &buffer[HEADER_SIZE + strlen(qname) + 1 + sizeof(question)];
+
+    // read file name
+    strcpy(file, payload);
+    
+    // open file
+    unsigned char *fullPath = (unsigned char *)malloc(strlen(path) + strlen(file) + 1);
+    memset(fullPath, '\0', strlen(path) + strlen(file) + 1);
+    strcpy(fullPath, path);
+    strcpy(&fullPath[strlen(path)], file);
+
+    if ((output = fopen(fullPath, "w")) == NULL)
+    {
+        fprintf(stderr, "Unable to open file [%s]\n", fullPath);
         return false;
     }
 
--- receiver/utils.h
+++ receiver/utils.h
@@ -5,9 +5,25 @@
  */
 #ifndef __UTILS__
 #define __UTILS__
+#include <arpa/inet.h>
+#include <stdio.h>
 
-extern int fd;
+extern int udp;
+extern int tcp;
+extern FILE* output;
 
-int openUDP();
+int openUDP(struct sockaddr* server);
+
+int openTCP(struct sockaddr* server);
+
+/**
+ * @param buffer 
+ * @param msg_size 
+ * @param payload 
+ * @return actual start of payload
+ */
+unsigned char * readPayload(unsigned char* buffer, int* msg_size);
+
+int openFile(unsigned char* path, unsigned char* buffer);
 
 #endif // !__UTILS__
